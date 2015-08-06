3f0eb2f52d45a5dcbe157b11cfe9a63b182c21d4
paveytel <paveytel@buffalo.edu>
2015-08-05 23:45:37 -0500

    added follow buttons and set onclicklsiteners

--- app/src/main/java/com/paveynganpi/ballonor/ui/UserProfileActivity.java
+++ app/src/main/java/com/paveynganpi/ballonor/ui/UserProfileActivity.java
@@ -1,11 +1,18 @@
 package com.paveynganpi.ballonor.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.parse.ParseUser;
 import com.paveynganpi.ballonor.R;
@@ -19,9 +26,28 @@ import butterknife.InjectView;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserProfileActivity extends AppCompatActivity {
-    @InjectView(R.id.profile_image) CircleImageView mProfileImage;
-    @InjectView(R.id.userProfileTabs) SlidingTabLayout mUserProfileTabs;
-    @InjectView(R.id.userProfilePager) ViewPager mUserProfilePager;
+    @InjectView(R.id.profile_image)
+    CircleImageView mProfileImage;
+    @InjectView(R.id.userProfileTabs)
+    SlidingTabLayout mUserProfileTabs;
+    @InjectView(R.id.userProfilePager)
+    ViewPager mUserProfilePager;
+    @InjectView(R.id.userProfileScreenName)
+    TextView mUserProfileScreenName;
+    @InjectView(R.id.userProfileFullName)
+    TextView mUserProfileFullName;
+    @InjectView(R.id.followersCount)
+    TextView mFollowersCount;
+    @InjectView(R.id.followersLable)
+    TextView mFollowersLable;
+    @InjectView(R.id.followingCount)
+    TextView mFollowingCount;
+    @InjectView(R.id.followingLable)
+    TextView mFollowingLable;
+    @InjectView(R.id.profile_image_edit_view)
+    ImageView mProfileImageEditView;
+    @InjectView(R.id.followImageButton)
+    ImageButton mFollowImageButton;
     private Toolbar mToolbar;
     protected ParseUser mCurrentUser;
 
@@ -43,7 +69,45 @@ public class UserProfileActivity extends AppCompatActivity {
         Picasso.with(this)
                 .load(profileImageUrl)
                 .into(mProfileImage);
+
+        mProfileImageEditView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(UserProfileActivity.this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
+        mFollowImageButton.setOnClickListener(mFollowImageButtonListener);
     }
+    
+    protected AdapterView.OnClickListener mFollowImageButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(!mFollowImageButton.isSelected()){
+                mFollowImageButton.setSelected(true);
+                mFollowImageButton.setImageResource(R.drawable.ic_account_check);
+                mFollowImageButton.setBackgroundResource(R.drawable.account_checked_button_shape);
+
+            }
+            else if (mFollowImageButton.isSelected()){
+                mFollowImageButton.setSelected(false);
+                mFollowImageButton.setImageResource(R.drawable.ic_account_plus);
+                mFollowImageButton.setBackgroundResource(R.drawable.account_button_plus_shape);
+            }
+        }
+    };
+
+    protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

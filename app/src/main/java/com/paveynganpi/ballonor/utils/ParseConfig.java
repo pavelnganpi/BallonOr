8ef20983edc090b68a96adfc4ac44acb2931025e
paveytel <paveytel@buffalo.edu>
2015-08-10 23:42:06 -0500

    update gitignore to ignore parseConfig

--- app/src/main/java/com/paveynganpi/ballonor/utils/ParseConfig.java
+++ app/src/main/java/com/paveynganpi/ballonor/utils/ParseConfig.java
@@ -9,5 +9,4 @@ public class ParseConfig {
     public static String ParseClientKey = "4FBSGsODwvCMXq9XmGBcbdggfShPaUPAxVPxp5uM";
     public static String TwitterConsumerKey = "rwzNdxb3SZ6UZBpuWq2HkyUu6";
     public static String ConsumerSecret = "bhP9OAvAnkyc13FLSrdrs7ixfcIYIeMeD1v0TqO8a7hJtmjvcp";
-
 }

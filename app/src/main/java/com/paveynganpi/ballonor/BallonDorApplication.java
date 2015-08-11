2237b94daa84683220fe686535f49975abb395d6
paveytel <paveytel@buffalo.edu>
2015-08-10 23:28:21 -0500

    refactored private keys into a config file

--- app/src/main/java/com/paveynganpi/ballonor/BallonDorApplication.java
+++ app/src/main/java/com/paveynganpi/ballonor/BallonDorApplication.java
@@ -6,6 +6,7 @@ import com.parse.Parse;
 import com.parse.ParseInstallation;
 import com.parse.ParseTwitterUtils;
 import com.parse.ParseUser;
+import com.paveynganpi.ballonor.utils.ParseConfig;
 import com.paveynganpi.ballonor.utils.ParseConstants;
 
 /**
@@ -18,13 +19,13 @@ public class BallonDorApplication extends Application {
         super.onCreate(); //doing since overing from the parent, so we want o inherit from the base class too
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "Skm8cEztx4rcXdhIGrHPjic7mVVS3xrhMr6Jhuvy", "4FBSGsODwvCMXq9XmGBcbdggfShPaUPAxVPxp5uM");
+        Parse.initialize(this, ParseConfig.ParseApplicationId, ParseConfig.ParseClientKey);
         ParseInstallation.getCurrentInstallation().saveInBackground();
         //facebook initialization
         //ParseFacebookUtils.initialize(this);
 
         //twitter initialization
-        ParseTwitterUtils.initialize("rwzNdxb3SZ6UZBpuWq2HkyUu6", "bhP9OAvAnkyc13FLSrdrs7ixfcIYIeMeD1v0TqO8a7hJtmjvcp");
+        ParseTwitterUtils.initialize(ParseConfig.TwitterConsumerKey, ParseConfig.ConsumerSecret);
     }
     public static void updateParseInstallation(ParseUser parseUser){
         ParseInstallation parseInstallation = ParseInstallation.getCurrentInstallation();
---
+++ app/src/main/java/com/paveynganpi/ballonor/utils/ParseConfig.java
@@ -0,0 +1,13 @@
+package com.paveynganpi.ballonor.utils;
+
+/**
+ * Created by paveynganpi on 8/10/15.
+ */
+public class ParseConfig {
+
+    public static String ParseApplicationId = "Skm8cEztx4rcXdhIGrHPjic7mVVS3xrhMr6Jhuvy";
+    public static String ParseClientKey = "4FBSGsODwvCMXq9XmGBcbdggfShPaUPAxVPxp5uM";
+    public static String TwitterConsumerKey = "rwzNdxb3SZ6UZBpuWq2HkyUu6";
+    public static String ConsumerSecret = "bhP9OAvAnkyc13FLSrdrs7ixfcIYIeMeD1v0TqO8a7hJtmjvcp";
+
+}
